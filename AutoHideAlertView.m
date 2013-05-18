17dc450a7ded55cdf5058450fd6db037652d4291
winlin <pcliuguangtao@163.com>
2013-05-18 14:35:02 +0800

    Create AutoHideAlertView.m

---
+++ AutoHideAlertView.m
@@ -0,0 +1,47 @@
+//
+//  AutoHideAlertView.m
+//  test
+//
+//  Created by gtliu on 5/17/13.
+//  Copyright (c) 2013 wicresoft.com. All rights reserved.
+//
+
+#import "AutoHideAlertView.h"
+
+@implementation AutoHideAlertView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+-(void)show
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(autoHideWillResignActive:) name:UIApplicationWillResignActiveNotification object:nil];
+    [super show];
+}
+
+-(void)autoHideWillResignActive:(NSNotification *)noti
+{
+    [self dismissWithClickedButtonIndex:0 animated:YES];
+}
+
+-(void)dismissWithClickedButtonIndex:(NSInteger)buttonIndex animated:(BOOL)animated
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIApplicationWillResignActiveNotification object:nil];
+    [super dismissWithClickedButtonIndex:buttonIndex animated:animated];
+}
+@end
